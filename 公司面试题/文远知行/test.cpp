83a5ab86a5483eab62b0dfb9ed472fc530a7590b
Yao <shine19960531@hotmail.com>
2021-07-29 18:07:51 +0800

    add questions of interviews

---
+++ 公司面试题/文远知行/test.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+// 给定和很多组括号，[](){}, 然后判断其中是否存在未配对的括号
+// 参考https://blog.csdn.net/weixin_41204899/article/details/81480267
---
+++ 公司面试题/远弗科技/test.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+
+// 给定一个二位数组，然后判断起是否存在，从做到右，从上到下都是依次递增的，然后判断一个数字是否存在于当前二位数组中
+// 参考剑指offer 第一题 https://cuijiahua.com/blog/2017/11/basis_1.html
